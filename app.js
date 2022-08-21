cadc0c89a2d544ca97de3958b5948c0b284d7a14
Artur Poffo <arturpoffoc@hotmail.com>
2022-08-21 18:32:53 -0300

    Procfile

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const server = app.listen(3000, () => console.log('listening on port ' + 3000));
+const server = app.listen(PORT, () => console.log('listening on port ' + PORT));
 
 const messages = [];
 
